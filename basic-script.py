945f502e74fb1ca0edb9b885146fc6a09b9fef23
Pavel Sankin <spdeveloper90@gmail.com>
2021-03-31 22:38:17 +0300

    more basic stuff

--- basic-script.py
+++ basic-script.py
@@ -1,5 +1,21 @@
 import psycopg2
-conn = psycopg2.connect(dbname='pankin', user='pankin',
-                        password='12zuhabo')
-cursor = conn.cursor()
-cursor.execute(open("db-init.sql", "r").read())
+def initialize_database():
+    global conn
+    global cursor
+    conn = psycopg2.connect(dbname='pankin', user='pankin',
+                            password='12zuhabo')
+    cursor = conn.cursor()
+    cursor.execute(open("db-init.sql", "r").read())
+    return cursor
+
+initialize_database()
+while True:
+    print("Type an SQL query or 'q' to quit.")
+    query = input()
+    if query == 'q':
+        break
+    cursor.execute(query)
+    print(cursor.fetchall())
+
+cursor.close()
+conn.close()
--- db-init.sql
+++ db-init.sql
@@ -9,7 +9,10 @@ create table education (
 );
 drop table if exists contacts cascade;
 create table contacts (
-	
+	id numeric(5) constraint fk_c_ppl references people(id) not null,
+	linkedin varchar(250),
+	github varchar(250),
+	email varchar(250)
 );
 drop table if exists jobs cascade;
 create table jobs (
@@ -23,4 +26,7 @@ drop table if exists roles cascade;
 create table roles (
 
 );
-
+insert into people (id, name) values (1, 'Pavel Sankin');
+insert into people (id, name) values (2, 'Andrei Daletski');
+insert into contacts (id, linkedin, github, email) values (1, 'https://www.linkedin.com/in/pavel-sankin-bbaa371bb/', 'github.com/Pankin610', 'pavelsankin610@gmail.com');
+insert into contacts (id, linkedin, github) values (2, 'https://www.linkedin.com/in/andrei-daletski-490b29205/', 'github.com/DANDROZAVR');
