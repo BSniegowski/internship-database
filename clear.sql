fe5f3896ad9f558425beec3b5941c6c525a044d0
Piotr <piotr.kubaty@student.uj.edu.pl>
2021-04-07 23:55:28 +0200

    zmienilem create i dodalem clear

---
+++ clear.sql
@@ -0,0 +1,9 @@
+drop table if exists people cascade;
+drop table if exists companies cascade;
+drop table if exists majors cascade;
+drop table if exists education cascade;
+drop table if exists contacts cascade;
+drop table if exists roles cascade;
+drop table if exists work_place cascade;
+drop table if exists jobs cascade;
+drop table if exists recommendations cascade;
