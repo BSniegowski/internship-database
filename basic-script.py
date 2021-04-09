7adafc34928423b3e700006cbeefcbdc203ce6d6
Pavel Sankin <spdeveloper90@gmail.com>
2021-04-09 21:10:55 +0300

    basic UI

--- basic-script.py
+++ basic-script.py
@@ -1,4 +1,7 @@
 import psycopg2
+from tkinter import *
+
+
 def initialize_database():
     global conn
     global cursor
@@ -8,14 +11,22 @@ def initialize_database():
     cursor.execute(open("db-init.sql", "r").read())
     return cursor
 
-initialize_database()
-while True:
-    print("Type an SQL query or 'q' to quit.")
-    query = input()
-    if query == 'q':
-        break
-    cursor.execute(query)
-    print(cursor.fetchall())
 
-cursor.close()
-conn.close()
+def read_query():
+    query = query_input.get()
+    cursor.execute(query)
+    output_label.config(text=cursor.fetchall())
+    query_input.delete(0, END)
+
+
+# read_queries()
+initialize_database()
+root = Tk()
+Label(root, text="The internship database.").grid(row=0)
+Label(root, text="Type an SQL query and press 'Execute' ").grid(row=1)
+output_label = Label(root, text="")
+output_label.grid(row=2)
+Button(root, text="Execute", command=read_query).grid(row=1, column=1)
+query_input = Entry(root, width=70)
+query_input.grid(row=3)
+root.mainloop()
