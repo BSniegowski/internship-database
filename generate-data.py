15a3de9d032e8baa71b29ef7a5e53924c2e04017
Pavel Sankin <pavelsankin610@gmail.com>
2021-04-21 00:46:54 +0300

    role generator

--- generate-data.py
+++ generate-data.py
@@ -1,6 +1,9 @@
 from internship_database import *
 import random
 import numpy as np
+from faker import Faker
+
+fake = Faker()
 
 initializeDatabase()
 with open("generated-data.sql", "w") as clear_up:
@@ -19,7 +22,7 @@ def generate_people(amount):
         return first_names[random.randrange(len(first_names))] + ' ' + \
                last_names[random.randrange(len(last_names))]
 
-    with open("generated-data.sql", "a") as gd:
+    with open("insert_people.sql", "a") as gd:
         for i in range(amount):
             max_id += 1
             gd.write("insert into people (id, name) values (" + str(max_id) + ", '" + getRandomName() + "');\n")
@@ -37,7 +40,6 @@ def randomRevenue():
 
 
 def add_companies():
-    # companies = getQueryResult('select * from companies;')
     id = 0
     with open("generated-data.sql", "a") as gd:
         for company in open("companies.txt", "r").read().split('\n'):
@@ -45,6 +47,34 @@ def add_companies():
                      + str(id) + ", '" + company.replace("'", "") + "', '" + randomCountry() + "', " +
                      str(randomRevenue()) + ");\n")
             id += 1
+
+
+def getRandomDate(start='-30y'):
+    return fake.date_between(start_date=start, end_date='today')
+
+
+def randomListValue(list):
+    return list[random.randrange(len(countries))]
+
+
+def add_roles(amount):
+    role_names = []
+    for name in open("role_names.txt", "r").read().split('\n'):
+        role_names.append(name)
+    possible_hours = [20, 30, 40, 60]
+    company_ids = getQueryResult('select id from companies;')
+
+    with open("insert_jobs.sql", "a") as roles:
+        for id in range(amount):
+            role_name = randomListValue(role_names)
+            hours = randomListValue(possible_hours)
+            salary_min = randomRevenue()
+            salary_max = randomRevenue()
+            if salary_min > salary_max:
+                salary_min, salary_max = salary_max, salary_min
+
+            roles.write("insert into roles (role_id, role_name, company_id, hours, salary_range_min, salary_range_max)"
+                        "values (%s, %s, %s, %s, %s, %s)" % id, role_name, company_ids, hours, salary_min, salary_max)
 
 
 add_companies()
